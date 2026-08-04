e2def66684d526fb4ecafc9de92420ce15030c80
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-08-05 00:43:31 +0800

    Added declaration in cafe class

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/Cafe.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/Cafe.java
@@ -3,5 +3,7 @@ package ph.edu.dlsu.lbycpob.animotaftfoodbites.model.oop;
 import java.util.List;
 
 public class Cafe extends FoodPlace implements CafeCategorable {
+    private boolean hasWifi;
+
 }
 
