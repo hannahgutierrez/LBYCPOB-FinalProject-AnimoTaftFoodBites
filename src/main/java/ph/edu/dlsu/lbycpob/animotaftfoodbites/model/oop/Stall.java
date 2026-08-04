c1d5cc0243af368791fd009716a5fc45d674c133
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-08-05 01:03:12 +0800

    Added constructors for Stall class

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/Stall.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/Stall.java
@@ -3,6 +3,17 @@ package ph.edu.dlsu.lbycpob.animotaftfoodbites.model.oop;
 import java.util.List;
 
 public class Stall extends FoodPlace {
+    //Constructors
+    public Stall(String id, String name, String category, String address,
+                 String contactInfo, String openingHours, String logoPath,
+                 String storeImagePath, List<String> menuImagePaths, String mapUrl) {
+        super(id, name, category, address, contactInfo, openingHours, logoPath, storeImagePath, menuImagePaths, mapUrl);
+    }
+
+
+    public Stall(String name, String address, String contactNumber, String operationHours) {
+        super(name, address, contactNumber, operationHours);
+    }
 
 }
 
