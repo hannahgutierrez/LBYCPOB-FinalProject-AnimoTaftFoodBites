36b5fd29cc9c31b362a487bb7f9dd2c3da5a7e89
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-08-05 00:27:08 +0800

    Created an interface for restaurant

---
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/RestaurantsCategorable.java
@@ -0,0 +1,7 @@
+package ph.edu.dlsu.lbycpob.animotaftfoodbites.model.oop;
+
+public interface RestaurantsCategorable {
+    String getCuisineType();
+    void setCuisineType(String cuisineType);
+}
+
