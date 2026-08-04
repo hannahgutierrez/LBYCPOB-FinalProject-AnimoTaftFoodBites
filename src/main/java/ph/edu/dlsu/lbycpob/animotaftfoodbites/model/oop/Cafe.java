e307be58ce332066502fcb8419771662798e2bac
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-08-05 00:44:42 +0800

    Created constructor in cafe class

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/Cafe.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/Cafe.java
@@ -5,5 +5,17 @@ import java.util.List;
 public class Cafe extends FoodPlace implements CafeCategorable {
     private boolean hasWifi;
 
+    public Cafe(String id, String name, String category, String address,
+                String contactInfo, String openingHours, String logoPath,
+                String storeImagePath, List<String> menuImagePaths, String mapUrl,
+                boolean hasWifi) {
+        super(id, name, category, address, contactInfo, openingHours, logoPath, storeImagePath, menuImagePaths, mapUrl);
+        this.hasWifi = hasWifi;
+    }
+
+    public Cafe(String name, String address, String contactNumber, String operationHours, boolean hasWifi) {
+        super(name, address, contactNumber, operationHours);
+        this.hasWifi = hasWifi;
+    }
 }
 
