6a64ecb8cf08d0b1bb1607b88100aa8e38aa6280
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-08-05 00:52:17 +0800

    Added constructors in Restaurant class

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/Restaurant.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/Restaurant.java
@@ -4,5 +4,19 @@ import java.util.List;
 
 public class Restaurant extends FoodPlace implements RestaurantsCategorable {
     private String cuisineType;
+
+    //Contructors
+    public Restaurant(String id, String name, String category, String address,
+                      String contactInfo, String openingHours, String logoPath,
+                      String storeImagePath, List<String> menuImagePaths, String mapUrl,
+                      String cuisineType) {
+        super(id, name, category, address, contactInfo, openingHours, logoPath, storeImagePath, menuImagePaths, mapUrl);
+        this.cuisineType = cuisineType;
+    }
+
+    public Restaurant(String name, String address, String contactNumber, String operationHours, String cuisineType) {
+        super(name, address, contactNumber, operationHours);
+        this.cuisineType = cuisineType;
+    }
 }
 
