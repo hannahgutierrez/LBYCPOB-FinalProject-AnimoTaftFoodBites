49f7715615c2d889272c7977714ef2bd02e12b45
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-08-05 00:32:13 +0800

    Added declarations in FoodPlace class

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/FoodPlace.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/FoodPlace.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class FoodPlace {
+    private String id;
+    private String name;
+    private String category;
+    private String address;
+    private String contactInfo;
+    private String openingHours;
+    private String logoPath;
+    private String storeImagePath;
+    private List<String> menuImagePaths;
+    private String mapUrl;
 
 }
 
