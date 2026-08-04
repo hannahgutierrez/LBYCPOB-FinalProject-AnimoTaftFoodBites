57a242d9fbad274126c7cc149a219ee969f1fd1f
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-08-05 01:04:01 +0800

    Implemented getCategoryName() method in Stall class

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/Stall.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/oop/Stall.java
@@ -9,11 +9,14 @@ public class Stall extends FoodPlace {
                  String storeImagePath, List<String> menuImagePaths, String mapUrl) {
         super(id, name, category, address, contactInfo, openingHours, logoPath, storeImagePath, menuImagePaths, mapUrl);
     }
-
-
+    
     public Stall(String name, String address, String contactNumber, String operationHours) {
         super(name, address, contactNumber, operationHours);
     }
 
+    @Override
+    public String getCategoryName() {
+        return "STALL";
+    }
 }
 
