9aae6ba50a10150680c58cb88cf8f1be31884179
alexiabungay <alexia_bungay@dlsu.edu.ph>
2026-08-10 21:43:22 +0800

    [FoodPlaceEntity] Added fields

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/FoodPlaceEntity.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/FoodPlaceEntity.java
@@ -6,5 +6,17 @@ import java.util.UUID;
 
 
 public class FoodPlaceEntity {
+    private UUID id = UUID.randomUUID();
+    private String name;
+    private String address;
+    private String contactNumber;
+    private String operationHours;
+    private String category;
+    private String logoUrl;
+    private String frontViewUrl;
+    private String googleMapLink;
+    private String foodType;
+    private Boolean wifiAvailable = false;
+    private List<MenuItemEntity> menuItems = new ArrayList<>();
 
 }
