7bd5fe2773cf4c6ccb4ae52901933218fbb7c4dd
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-08-10 19:52:53 +0800

    Added Chowking restaurant details

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/controller/FoodPlaceController.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/controller/FoodPlaceController.java
@@ -85,6 +85,26 @@ public class FoodPlaceController {
                 mapsUrl,
                 "Fast Food"
         ));
+
+        // Chowking
+        List<String> chowkingMenus = new ArrayList<>();
+        for (int i = 1; i <= 33; i++) {
+            chowkingMenus.add("/images/Chowking/ChowkingMenu" + i + ".png");
+        }
+
+        foodPlaces.add(new Restaurant(
+                "2",
+                "Chowking",
+                "Fast Food",
+                "Level 1, D'Student's Place, Commercial Center, 2488 Taft Ave, Malate, Manila, 1004 Metro Manila",
+                "0939 722 0052",
+                "Operation Hours: Open 24 hours",
+                "/images/Chowking/ChowkingLogo.jpg",
+                "/images/Chowking/ChowkingPlacePhoto.png",
+                chowkingMenus,
+                "https://maps.app.goo.gl/Qhm4JWukNyJCkAV37",
+                "Fast Food"
+        ));
         renderRestaurantGrid(foodPlaces);
         updateAdminButtonUI();
     }
