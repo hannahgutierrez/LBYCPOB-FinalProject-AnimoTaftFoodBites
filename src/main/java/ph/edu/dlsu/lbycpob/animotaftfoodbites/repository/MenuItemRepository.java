4384a34966d5de5667746ca16f3f8b559a60cb07
Jey0428 <johnlancevalencia05@gmail.com>
2026-08-12 02:22:37 +0800

    add imports

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/repository/MenuItemRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/repository/MenuItemRepository.java
@@ -1,4 +1,7 @@
 package ph.edu.dlsu.lbycpob.animotaftfoodbites.repository;
 
+import ph.edu.dlsu.lbycpob.animotaftfoodbites.model.MenuItemEntity;
+import java.util.*;
+
 public class MenuItemRepository {
 }
