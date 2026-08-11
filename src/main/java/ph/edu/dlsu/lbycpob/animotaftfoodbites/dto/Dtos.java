7a9628fbcb656b24d7cb56becb22d1e7f39726f3
Jey0428 <johnlancevalencia05@gmail.com>
2026-08-11 23:41:21 +0800

    add food place creation request

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/dto/Dtos.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/dto/Dtos.java
@@ -8,4 +8,20 @@ import java.util.List;
 import java.util.UUID;
 
 public class Dtos {
+
+    public record CreateFoodPlaceRequest(
+            String name,
+            String address,
+            String contactNumber,
+            String operationHours,
+            String category,
+            String logoUrl,
+            String frontViewUrl,
+            String googleMapLink,
+            String foodType,
+            Boolean wifiAvailable
+    ) {}
+
+
+
 }
