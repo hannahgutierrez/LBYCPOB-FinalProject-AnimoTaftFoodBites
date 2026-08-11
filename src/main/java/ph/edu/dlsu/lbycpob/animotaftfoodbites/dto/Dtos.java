2595203ab0558baa15b51959a0ce00f651099791
Jey0428 <johnlancevalencia05@gmail.com>
2026-08-11 23:29:49 +0800

    create Dtos class structure

---
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/dto/Dtos.java
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.lbycpob.animotaftfoodbites.dto;
+
+import ph.edu.dlsu.lbycpob.animotaftfoodbites.model.FoodPlaceEntity;
+import ph.edu.dlsu.lbycpob.animotaftfoodbites.model.MenuItemEntity;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+public class Dtos {
+}
