f09594014298c331c5ec32192a1e146b69814606
Jey0428 <johnlancevalencia05@gmail.com>
2026-08-12 02:15:28 +0800

    add food place listing

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/repository/FoodPlaceRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/repository/FoodPlaceRepository.java
@@ -7,4 +7,13 @@ import java.util.stream.Collectors;
 
 public class FoodPlaceRepository {
     private final Map<UUID, FoodPlaceEntity> database = new LinkedHashMap<>();
+
+    public List<FoodPlaceEntity> findAllByOrderByNameAsc() {
+        return database.values().stream()
+                .sorted(Comparator.comparing(
+                        FoodPlaceEntity::getName,
+                        String.CASE_INSENSITIVE_ORDER
+                ))
+                .collect(Collectors.toList());
+    }
 }
