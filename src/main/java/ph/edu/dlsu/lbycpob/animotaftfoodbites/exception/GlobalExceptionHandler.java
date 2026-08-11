646ae135bf99b689c21646db45a8e16368d4d3dd
Jey0428 <johnlancevalencia05@gmail.com>
2026-08-12 01:22:07 +0800

    create JavaFX error alert

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/exception/GlobalExceptionHandler.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/exception/GlobalExceptionHandler.java
@@ -6,5 +6,10 @@ import javafx.scene.control.ButtonType;
 public class GlobalExceptionHandler {
 
     public static void handleException(Exception ex) {
+        Alert alert = new Alert(
+                Alert.AlertType.ERROR,
+                ex.getMessage(),
+                ButtonType.OK
+        );
     }
 }
