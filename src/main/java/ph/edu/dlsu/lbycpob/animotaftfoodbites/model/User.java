c11a49a821d4701ba38b7c72cd7cfa3ca35c4230
alexiabungay <alexia_bungay@dlsu.edu.ph>
2026-08-14 21:15:51 +0800

    [User] Added public User() and getter for Username and password

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/User.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/model/User.java
@@ -6,4 +6,13 @@ import java.io.Serializable;
 public class User implements Serializable {
     private String username;
     private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+
 }
