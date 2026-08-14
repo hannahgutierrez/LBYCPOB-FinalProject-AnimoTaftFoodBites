9a46135c0b98039cedb2c821ba4069d0c7bebbf1
alexiabungay <alexia_bungay@dlsu.edu.ph>
2026-08-14 20:23:42 +0800

    [DataStorage] Added fields

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/util/DataStorage.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/util/DataStorage.java
@@ -9,4 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataStorage {
+    private static final String FOOD_PLACES_FILE = "food_places.dat";
+    private static final String USERS_FILE = "users.dat";
 }
