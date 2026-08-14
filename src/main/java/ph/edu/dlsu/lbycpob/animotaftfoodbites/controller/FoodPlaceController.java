018b93ff355b856acaa87f141c4a68092b6fb86c
Jey0428 <johnlancevalencia05@gmail.com>
2026-08-15 03:14:11 +0800

    remove mcdo

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/controller/FoodPlaceController.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/controller/FoodPlaceController.java
@@ -81,24 +81,7 @@ public class    FoodPlaceController {
             categoryFilter.getItems().addAll("ALL", "RESTAURANT", "CAFE", "STALL");
             categoryFilter.setOnAction(e -> filterCategory());
         }
-        String mapsUrl = "https://maps.app.goo.gl/mqQ9qBPdVtFTUbFL6";
-        List<String> mcdoMenus = new ArrayList<>();
-        for (int i = 1; i <= 19; i++) {
-            mcdoMenus.add("/images/mcdo/McdoMenu" + i + ".png");
-        }
-        foodPlaces.add(new Restaurant(
-                "1",
-                "McDonald's Taft",
-                "Fast Food",
-                "2399 Taft Avenue, Malate, Manila",
-                "(02) 8888 6236",
-                "Operation Hours: 24-hours",
-                "/images/mcdo/McdoLogo.jpg",
-                "/images/mcdo/McdoPlacePhoto.jpg",
-                mcdoMenus,
-                mapsUrl,
-                "Fast Food"
-        ));
+
 
         // Chowking
         List<String> chowkingMenus = new ArrayList<>();
