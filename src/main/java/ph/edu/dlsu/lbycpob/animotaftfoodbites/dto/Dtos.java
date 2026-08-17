c64cb648bc41d1a7031ad910e2deed433bed2163
alexiabungay <alexia_bungay@dlsu.edu.ph>
2026-08-17 22:55:18 +0800

    [Dtos] Remove CreateFoodPlaceRequest()

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/dto/Dtos.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/dto/Dtos.java
@@ -9,18 +9,7 @@ import java.util.UUID;
 
 public class Dtos {
 
-    public record CreateFoodPlaceRequest(
-            String name,
-            String address,
-            String contactNumber,
-            String operationHours,
-            String category,
-            String logoUrl,
-            String frontViewUrl,
-            String googleMapLink,
-            String foodType,
-            Boolean wifiAvailable
-    ) {}
+
 
     public record CreateMenuItemRequest(
             String name,
