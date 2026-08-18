2d05d265f8fe43400fece08a4b9a695d28e1a4a7
alexiabungay <alexia_bungay@dlsu.edu.ph>
2026-08-18 11:39:55 +0800

    [FoodPlaceRepository] Remove findAllByOrderByNameAsc()

--- src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/repository/FoodPlaceRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animotaftfoodbites/repository/FoodPlaceRepository.java
@@ -6,16 +6,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class FoodPlaceRepository {
-    private final Map<UUID, FoodPlaceEntity> database = new LinkedHashMap<>();
-
-    public List<FoodPlaceEntity> findAllByOrderByNameAsc() {
-        return database.values().stream()
-                .sorted(Comparator.comparing(
-                        FoodPlaceEntity::getName,
-                        String.CASE_INSENSITIVE_ORDER
-                ))
-                .collect(Collectors.toList());
-    }
 
     public List<FoodPlaceEntity> findByCategoryIgnoreCaseOrderByNameAsc(String category) {
         return database.values().stream()
